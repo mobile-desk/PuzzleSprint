fb0405fc85bfba1829e8db64345c71ced9f26e59
Mobile Desk <63956765+mobile-desk@users.noreply.github.com>
2024-01-01 12:35:01 +0100

    Update

---
+++ jan10.py
@@ -0,0 +1,4 @@
+Problem:Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+Example:Input: nums = [1, 3, 5, 6], target = 5 Output: 2 (since the target 5 is found at index 2)
+Input: nums = [1, 3, 5, 6], target = 2 Output: 1 (since the target 2 should be inserted at index 1 to maintain sorting order)
